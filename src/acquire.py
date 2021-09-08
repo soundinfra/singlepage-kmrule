05b10b9e3829a089f7cb29e48b80b1f44df0ed27
David Griffin <david@soundinfra.com>
2021-09-07 17:26:20 -0700

    acquire.py: Implement test_get_csv_unauthorized and add types.

--- src/acquire.py
+++ src/acquire.py
@@ -10,23 +10,23 @@ AUTH_TOKEN = os.environ.get(TOKEN_ENV_VAR)
 OPTIONS = "OPTIONS"
 AUTHORIZATION = "Authorization"
 EMPTY_PATH = "/"
+UTF8 = "utf-8"
 
 
 class Acquire():
 
-    def __init__(self, conn):
+    def __init__(self, conn: http.client.HTTPSConnection):
         self.conn = conn
 
-    def read_remote_csv(self, token):
+    def read_remote_csv(self, token: str) -> list[str]:
         conn = self.conn
         try:
             conn.request(OPTIONS, EMPTY_PATH,
                          headers={AUTHORIZATION: f"Bearer {token}"})
             response = conn.getresponse()
             if response.status is HTTPStatus.OK:
-                return response.readlines()
+                return [line.decode(UTF8) for line in response.readlines()]
             else:
-                print(f"Oops, got a {response.status}")
+                raise RuntimeError(f"Oops, got a {response.status}")
         finally:
             conn.close()
-        return []
--- tests/test_acquire.py
+++ tests/test_acquire.py
@@ -1,10 +1,9 @@
-import http.client
 import unittest
 from http import HTTPStatus
 
 from src import Acquire
 
-from unittest.mock import patch, MagicMock, Mock
+from unittest.mock import patch
 
 
 class TestAcquire(unittest.TestCase):
@@ -15,21 +14,35 @@ class TestAcquire(unittest.TestCase):
         actual = mysite.conn
         self.assertEqual(mock_conn, actual)
 
-
     @patch("http.client.HTTPResponse")
     @patch("http.client.HTTPSConnection")
     def test_get_csv_success(self, mock_conn, mock_resp):
+        # Given
         mock_resp.status = HTTPStatus.OK
-        csv_lines = ["one", "two"]
-        mock_resp.readlines.return_value = csv_lines
+        mock_resp.readlines.return_value = [b"one", b"two"]
         mock_conn.getresponse.return_value = mock_resp
 
+        # When
         mysite = Acquire(mock_conn)
         result = mysite.read_remote_csv("token")
-        self.assertEqual(result, csv_lines)
+
+        # Then
+        self.assertEqual(result, ["one", "two"])
         mock_conn.getresponse.assert_called_once()
         mock_resp.readlines.assert_called_once()
 
-    def get_csv_unauthorized(self):
-        notasite = Acquire("notasite.com")
-        notasite.read_remote_csv("token")
+    @patch("http.client.HTTPResponse")
+    @patch("http.client.HTTPSConnection")
+    def test_get_csv_unauthorized(self, mock_conn, mock_resp):
+        # Given
+        mock_resp.status = HTTPStatus.UNAUTHORIZED
+        mock_conn.getresponse.return_value = mock_resp
+
+        # When
+        notasite = Acquire(mock_conn)
+        with self.assertRaises(RuntimeError) as context:
+            notasite.read_remote_csv("token")
+
+        # Then
+        mock_resp.readlines.assert_not_called()
+        self.assertEqual("Oops, got a 401", str(context.exception))
